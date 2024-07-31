25bec36964c4236c3dbd1d9d90574b331ab2251f
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2024-07-31 01:33:10 -0700

    V0.1

--- test.py
+++ test.py
@@ -3,13 +3,18 @@ import enchant
 # Create a dictionary object for English (US)
 d = enchant.Dict('en_US')
 
+version = str(0.2)
+print('Decryptor V' + version)
+
 text_input = input(str('\n' + 'Enter text for decryption:' + ' \n'))
 
 
 
 def decryptor(word):
     word_split = []
-    # ayrikyay
+    l = 1
+    original = ''
+    # piss is isspay
     for x in word:
         word_split.append(x)
     for y in range(2):
@@ -20,15 +25,34 @@ def decryptor(word):
         return word
     else:
         while True:
-            last_element = word_split.pop()
-            word_split.insert(0, last_element)
-            word = "".join(word_split)
-            if d.check(word):
-                return word
-            else:
-                print('not a real word!')
+            if l == 4:
+                print('Error, no word detected.')
                 break
-    return word_split
+            rearLetters = len(word_split) - l
+            cutSplit = word_split[:rearLetters]
+            moveLetters = word_split[rearLetters:]
+            print(cutSplit)
+            print(moveLetters)
+            extended_list = moveLetters + cutSplit
+            print(extended_list)
+            fullWord = "".join(extended_list)
+            print(fullWord)
+            if d.check(fullWord):
+                print(fullWord, 'is a real word.')
+                return fullWord
+            else:
+                print(f"'{fullWord}' is not a real word.")
+                l += 1
+
+
+
+
+
+
+
+
+
+
 def sentence(text_input):
     final_sentence = []
     sentence_initial = text_input.split()
