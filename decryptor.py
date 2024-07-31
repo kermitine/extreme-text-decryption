da39b9da7b498572ae96bfb023cf8742e81134fa
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2024-07-31 12:46:06 -0700

    V1.2 -made improvements to overall experience -added punctuation sorting, as opposed to just filtering and removing it -artificially slowed down script so console logs are shown easier

--- decryptor.py
+++ decryptor.py
@@ -3,7 +3,7 @@ import time
 # Create a dictionary object for English (US)
 d = enchant.Dict('en_US')
 
-version = str(1.1)
+version = str(1.2)
 
 print('''                                                                                                       
                                                     ###%%%#*                                           
@@ -61,6 +61,7 @@ print(''' __                             .__   __   .__
 
 
 print('Decryptor V' + version)
+print('Powered by PyEnchant')
 
 text_input = input(str('\n' + 'Enter text for decryption:' + ' \n'))
 punctuation = ['!', '?', '.', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '#', '/', ':', ';',
@@ -71,30 +72,35 @@ nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 def decryptor(word):
     word_split = []
     pop_list = []
+    puncList = []
+    failcount = 1
     joined_word = ''.join(word)
     l = 1
     original = ''
     for x in word: #
         if x in nums: # instantly returns if number
             return word
-        word_split.append(x) # converts all strings to lowercase
+        word_split.append(x)
     for x in range(len(word_split)):
-        if word_split[x] in punctuation: # filters and removes punctuation
-            pop_list.append(x)
-        word_split[x] = word_split[x].lower()
-    if pop_list:
-        for x in pop_list:
+        word_split[x] = word_split[x].lower() # converts all strings to lowercase
+    for x in range(len(word_split)):
+        if word_split[x] in punctuation:
+            print('Punctuation detected, sorting')
+            puncList.append(word_split[x])
             word_split.pop(x)
-            print('Filtering punctuation!')
+
 
     for y in range(2):
         word_split.pop()
     if word_split[len(word_split)-1] == 'y': # considers the word originally as a vowel
-        print('Processing', joined_word)
+        print('Processing', joined_word + '_' + str(failcount))
         word_split.pop()
         word = "".join(word_split)
         print(word_split)
         print("'" + word + "'" + ' is a recognized word.' + '\n')
+        if puncList:
+            word_split = word_split + puncList
+            word = "".join(word_split)
         return word
     else:
         while True:
@@ -104,19 +110,24 @@ def decryptor(word):
             rearLetters = len(word_split) - l
             cutSplit = word_split[:rearLetters]
             moveLetters = word_split[rearLetters:]
-            print('Processing', joined_word)
+            print('Processing', joined_word + '_' + str(failcount))
             print(word_split)
             print(moveLetters)
             print(cutSplit)
             extended_list = moveLetters + cutSplit
             fullWord = "".join(extended_list)
-            print(fullWord )
+            print(fullWord)
             if d.check(fullWord):
                 print("'" + fullWord + "'" + ' is a recognized word.' + '\n')
+                if puncList:
+                    print('Punctuation detected, sorting')
+                    extended_list = extended_list + puncList
+                    fullWord = "".join(extended_list)
                 return fullWord
             else:
                 print("'" + fullWord + "'" ' is not a recognized word.' + '\n')
                 l += 1
+                failcount += 1
 
 
 
@@ -134,6 +145,7 @@ def sentence(text_input):
 
     for x in sentence_initial:
         final_sentence.append(decryptor(x))
+        time.sleep(1.5)
 
     joined_sentence = " ".join(final_sentence)
     return joined_sentence
