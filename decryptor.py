90e16cb9c26c3a6d6d0f49e888f8bf55f2cc3b68
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2024-07-31 20:11:32 -0700

    V1.4.4 -updated variable naming to be in line with PEP-8

--- decryptor.py
+++ decryptor.py
@@ -3,7 +3,7 @@ import time
 # Create a dictionary object for English (US)
 d = enchant.Dict('en_US')
 
-version = str('1.4.3')
+version = str('1.4.4')
 
 print('''                                                                                                       
                                                     ###%%%#*                                           
@@ -72,10 +72,10 @@ nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 def decryptor(word):
     word_split = []
-    puncList = []
-    failcount = 1
+    punc_list = []
+    fail_count = 1
     joined_word = ''.join(word)
-    l = 1
+    end_consonants = 1
     for x in word: #
         if x in nums: # instantly returns if number
             return word
@@ -85,48 +85,48 @@ def decryptor(word):
     for x in range(len(word_split)):
         if word_split[x] in punctuation:
             print('Punctuation detected, sorting')
-            puncList.append(word_split[x])
+            punc_list.append(word_split[x])
             word_split.pop(x)
 
 
     for y in range(2):
         word_split.pop()
     if word_split[len(word_split)-1] == 'y': # considers the word originally as a vowel
-        print('Processing', joined_word + '_' + str(failcount))
+        print('Processing', joined_word + '_' + str(fail_count))
         word_split.pop()
         word = "".join(word_split)
         print(word_split)
         print("'" + word + "'" + ' is a recognized word.' + '\n')
-        if puncList:
-            word_split = word_split + puncList
+        if punc_list:
+            word_split = word_split + punc_list
             word = "".join(word_split)
         return word
     else:
         while True:
-            if l == 5:
+            if end_consonants == 5:
                 print("'" + joined_word + "'" ' failed to decrypt' + '\n')
                 return('error')
-            rearLetters = len(word_split) - l
-            cutSplit = word_split[:rearLetters]
-            moveLetters = word_split[rearLetters:]
-            print('Processing', joined_word + '_' + str(failcount))
+            rear_letters = len(word_split) - end_consonants
+            cut_split = word_split[:rear_letters]
+            move_letters = word_split[rear_letters:]
+            print('Processing', joined_word + '_' + str(fail_count))
             print(word_split)
-            print(moveLetters)
-            print(cutSplit)
-            extended_list = moveLetters + cutSplit
-            fullWord = "".join(extended_list)
-            print(fullWord)
-            if d.check(fullWord):
-                print("'" + fullWord + "'" + ' is a recognized word.' + '\n')
-                if puncList:
+            print(move_letters)
+            print(cut_split)
+            extended_list = move_letters + cut_split
+            full_word = "".join(extended_list)
+            print(full_word)
+            if d.check(full_word):
+                print("'" + full_word + "'" + ' is a recognized word.' + '\n')
+                if punc_list:
                     print('Punctuation detected, sorting')
-                    extended_list = extended_list + puncList
-                    fullWord = "".join(extended_list)
-                return fullWord
+                    extended_list = extended_list + punc_list
+                    full_word = "".join(extended_list)
+                return full_word
             else:
-                print("'" + fullWord + "'" ' is not a recognized word.' + '\n')
-                l += 1
-                failcount += 1
+                print("'" + full_word + "'" ' is not a recognized word.' + '\n')
+                end_consonants += 1
+                fail_count += 1
 
 
 
@@ -165,17 +165,17 @@ def loopStart(text_input):
         if x == 'error':
             print('Errors detected. Check log for anything verification could have missed.' + '\n')
             break
-    exitCode = input(str('\n' + 'Enter t to translate another sentence. Enter any other key to exit.' + '\n'))
-    return exitCode
+    input_code = input(str('\n' + 'Enter t to translate another sentence. Enter any other key to exit.' + '\n'))
+    return input_code
 
 
 # Continuous code loop
-exitCode1 = str(loopStart(text_input))  # calls loopStart() which calls all other functions. loopstart() returns exitcode
+exit_code = str(loopStart(text_input))  # calls loopStart() which calls all other functions. loopstart() returns input_code
 
 while True:
-    if exitCode1 in ['t', 'T']:
+    if exit_code in ['t', 'T']:
         text_input = input(str('\n' + 'Enter text for decryption:' + ' \n'))
-        exitCode1 = loopStart(text_input)
+        exit_code = loopStart(text_input)
     else:
         print('Program Terminating...')
         time.sleep(1.5)
